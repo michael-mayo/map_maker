05897393e30f8f6cd7b81776ba98ae5f232a6bcb
Michael Mayo <michaelmayo704@gmail.com>
2025-10-08 12:07:46 +1300

    added debugging feature; added method specifically to save in cs2 format

--- noise.py
+++ noise.py
@@ -47,20 +47,27 @@ class NoiseStack:
     def __init__(self,
                  seed:int=42,
                  octaves:int=8,
-                 size:int=4096):
+                 size:int=4096,
+                 debug:bool=False):
         self._seed=seed
         self._octaves=octaves
         self._size=size
         self._rng=np.random.default_rng(self._seed)
         seeds=self._rng.integers(0,high=2**16,size=octaves)
+        offsets=(self._rng.uniform(low=-1000,high=1000,size=octaves*2)
+                 .reshape(octaves,2))
         self._layers=[Noise(seed=int(seeds[i]),
                             size=self._size,
                             amplitude=1.0/(2**i),
-                            frequency=2**i)
+                            frequency=2**i,
+                            offset=tuple(offsets[i,:]))
                       for i in range(octaves)]
         self._noise=self._layers[0]._noise.copy()
         for i in range(1,octaves):
             self._noise+=self._layers[i]._noise
+        if debug:
+            for l in self._layers:
+                print(l)
 
     def __str__(self):
         """ Stringifier """
@@ -77,8 +84,7 @@ class NoiseStack:
             "noise_mean": self._noise.mean()
         }
 
-    def to_png(self,
-               filename:str,
+    def to_cs2_png(self,
                min_z:float=0.0,
                max_z:float=1.0):
         """ Save noise to a 16 bit png image """
@@ -87,10 +93,17 @@ class NoiseStack:
                 /(stats["noise_max"]-stats["noise_min"]))
         result*=(max_z-min_z)
         result+=min_z
-        cv2.imwrite(filename,(result*(2**16-1)).astype(np.uint16))
+        wm=(result * (2 ** 16 - 1)).astype(np.uint16)
+        if self._size!=4096:
+            wm=cv2.resize(wm,(4096,4096),interpolation=cv2.INTER_CUBIC)
+        offset=4096//2+1024//2
+        hm=wm[offset:(offset+1024),offset:(offset+1024)]
+        hm=cv2.resize(hm,(4096,4096),interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite(f"wm_{self._seed}.png",wm)
+        cv2.imwrite(f"hm_{self._seed}.png",hm)
+
 
 # Tester code
-n=NoiseStack()
+n=NoiseStack(seed=818,debug=False)
 print(n)
-n.to_png("img0.png")
-n.to_png("img1.png",min_z=0.3,max_z=0.8)
+n.to_cs2_png(min_z=0.0,max_z=1.0)
