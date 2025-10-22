7c538ea5487fbfd717b46877ae50fb8cf3a543fa
Michael Mayo <michaelmayo704@gmail.com>
2025-10-22 17:32:48 +1300

    tidied up repo; added readme

---
+++ README.md
@@ -0,0 +1,22 @@
+
+## Map Generator
+
+This program uses simplex noise to generate map heightmaps suitable for the City Skylines 2 Map Editor.
+
+To set everything up:
+* install a python3 virtual environment
+* clone the map maker repo
+* use pip to install required libraries, `pip install -r requirements.txt`
+
+To run the program using the following command:
+```
+python map.py --seed [SEED] --it [IT]
+```
+Replace SEED with your random number seed and IT with the number of maps you want.
+The program will generate pairs of PNG images, `map[n]_wm.png` and `map[n]_hm.png`
+being the world and height maps respectively. These files can be copied to your CS2 heightmaps
+folder (`C:\Users\USERNAME\AppData\LocalLow\Colossal Order\Cities Skylines II\Heightmaps` on my PC)
+and then imported in the map editor.
+
+
+
--- map.py
+++ map.py
@@ -9,25 +9,21 @@ parser.add_argument("--it", type=int, default=3, help="number of iterations")
 args = parser.parse_args()
 
 rng=np.random.default_rng(args.seed)
-def gauss(x,y,x0=0.5,y0=0.5,sigma=0.1,amp=1):
-    xf=((x-x0)**2)/(2*sigma**2)
-    yf=((y-y0)**2)/(2*sigma**2)
-    return amp*np.exp(-1*(xf+yf))
+
 for it in range(args.it):
     seed=rng.integers(low=0,high=2**16)
-    map=Noise(seed=seed,size=4096,center=1024)
-    map.center(standardise=True)
-    map.ptf(lambda v: v+np.exp(v))
-    x0,y0,amp,sig,k=rng.uniform(low=0,high=1,size=5)
-    map.glf(lambda v,x,y:
-            v+gauss(x,y,
-                    x0=x0 if k>=0.5 else 0,
-                    y0=y0 if k<0.5 else 0,
-                    amp=amp/2.0+0.1,
-                    sigma=sig/2+0.25)
-            )
+    map=Noise(seed=seed,
+              size=4096,
+              center=1024,
+              octaves=8,
+              base_freq=rng.uniform()*1.5+0.5)
+    map.center(method="mean", standardise=True)
+    map.center(method="center_pixel",standardise=False)
+    map.ptf(lambda v: v**3 if v>=0 else v)
     map.to_png(f"map{it}_wm.png",
+
                f"map{it}_hm.png") # done
     print(map)
+
 
 
