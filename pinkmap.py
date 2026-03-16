c5746586ffa92bc25d4cc7eb9ec6a2f08f309e8d
Michael Mayo <michaelmayo704@gmail.com>
2026-03-17 08:04:25 +1300

    added quality optimisation for selecting maps

--- pinkmap.py
+++ pinkmap.py
@@ -1,14 +1,14 @@
 
 import numpy as np
 import cv2
+import functools
 
 from typing import Tuple
 from scipy.ndimage import gaussian_filter
 
-RNG=np.random.Generator
-""" Random number generator"""
 
-def pink_noise_2d(shape:Tuple[int,int],
+def pink_noise_2d(rng:np.random.Generator,
+                  shape:Tuple[int,int],
                   beta:float=1.0):
     """ Generate 2D pink noise when beta=1.0 """
     h,w=shape
@@ -16,7 +16,7 @@ def pink_noise_2d(shape:Tuple[int,int],
     fx=np.fft.fftfreq(w)[None,:]
     f=np.sqrt(fx*fx+fy*fy)
     f[0,0]=1.0
-    spectrum=RNG.normal(size=(h,w))+1j*RNG.normal(size=(h,w))
+    spectrum=rng.normal(size=(h,w))+1j*rng.normal(size=(h,w))
     spectrum/=np.power(f,beta/2.0)
     field=np.fft.ifft2(spectrum).real
     field-=field.min()
@@ -45,15 +45,16 @@ def bilinear_sample(img:np.ndarray,
         sx*sy*v11
     )
 
-def domain_warped_pink_noise(shape:Tuple[int,int]=(256, 256),
+def domain_warped_pink_noise(rng:np.random.Generator,
+                             shape:Tuple[int,int]=(256, 256),
                              base_beta:float=4.0,
                              warp_beta:float=1.0,
                              warp_strength:float=60.0):
     """ Generate domain warped pink noise """
     h,w=shape
-    base=pink_noise_2d(shape,beta=base_beta)
-    warp_x=pink_noise_2d(shape,beta=warp_beta)
-    warp_y=pink_noise_2d(shape,beta=warp_beta)
+    base=pink_noise_2d(rng,shape,beta=base_beta)
+    warp_x=pink_noise_2d(rng,shape,beta=warp_beta)
+    warp_y=pink_noise_2d(rng,shape,beta=warp_beta)
     warp_x=2.0*warp_x-1.0
     warp_y=2.0*warp_y-1.0
     yy,xx=np.mgrid[0:h,0:w]
@@ -66,20 +67,30 @@ def domain_warped_pink_noise(shape:Tuple[int,int]=(256, 256),
 
 
 def create_map(
-        seed:int,
+        rng:np.random.Generator,
         shape:Tuple[int,int]=(256,256),
         smoothing_sigma:float=4)->np.ndarray:
     """ Create a map """
-    global RNG
-    RNG=np.random.default_rng(seed)
-    result=domain_warped_pink_noise(shape)
+    result=domain_warped_pink_noise(rng,shape)
     result=gaussian_filter(result,smoothing_sigma)
-    
-    # TODO: shift the height of the starting square down to 0.3
-    # TODO: flatten the starting square; height map is divided into 23x23 squares
-    # TODO: and the central 3x3 are the game start squares
-
     return result
+
+#@functools.lru_cache()
+def value_map(map:np.ndarray):
+    """ Value function for maps. Tries to position the starting square at a height
+        of approx 30% above the lowest height if possible """
+    h,w=map.shape
+    min,max=map.min(),map.max()
+    ch,cw=h//2,w//2
+    center_grey=(map[ch,cw]-min)/(max-min)
+    fs=8
+    g1=map[ch-fs,cw]-map[ch+fs,cw]
+    g2=map[ch,cw-fs]-map[ch,cw+fs]
+    g3=map[ch-fs,cw]-map[ch,cw+fs]
+    g4=map[ch,cw-fs]-map[ch+fs,cw]
+    center_gradient=np.sqrt(g1**2+g2**2+g3**3+g4**2)
+    return abs(center_grey-0.3)+5*center_gradient
+
 
 
 def save_map(filename_suffix:str,terrain:np.ndarray):
@@ -93,8 +104,19 @@ def save_map(filename_suffix:str,terrain:np.ndarray):
     hm=cv2.resize(hm,(4096,4096),interpolation=cv2.INTER_CUBIC)
     cv2.imwrite(f"example_maps/hm_{filename_suffix}.png", hm)
 
-if __name__=="__main__":
+def main():
     for seed in [42,22,111]:
-        save_map(
-            f"{seed}.png",
-            create_map(seed,shape=(512,512)))
+        rng=np.random.default_rng(seed)
+        best_map=None
+        for _ in range(10):
+            map=create_map(rng,shape=(512,512))
+            map_value=value_map(map)
+            print(seed,map_value)
+            if best_map is None or map_value<best_value:
+                best_map=map
+                best_value=map_value
+                print("best map updated")
+        save_map(f"{seed}.png",best_map)
+
+if __name__=="__main__":
+    main()
